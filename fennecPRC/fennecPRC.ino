7cf4a65de5f4e759fe84568c8d0c847da43d164f
Unknown <daniel.middleton@skybettingandgaming.com>
2020-08-14 18:01:24 +0100

    Automation

--- fennecPRC/fennecPRC.ino
+++ fennecPRC/fennecPRC.ino
@@ -1,5 +1,5 @@
 /*
-  fennecPRC v1.0
+  fennecPRC
   Programmable Recrystallization Chamber
   https://fennecfox.io/
 */
@@ -9,9 +9,12 @@
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 
+const float VERSION = 1.0;
 const int DEBUG = 0;
 
-// Temperature sensor.
+/*
+  Temperature sensor config.
+*/
 const int TEMPERATURE_PIN = 13;
 const int TEMPERATURE_PRECISION =
     10; // 9 (0.5C), 10 (0.25C), 11 (0.125C) or 12 (0.0625C) bits.
@@ -20,20 +23,25 @@ OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature sensors(&oneWire);
 DeviceAddress thermometerDeviceAddress;
 
-// H-bridge (for TEC).
+/*
+  H-bridge (for TEC) config.
+*/
 const int HBRIDGE_RPWM = 5;
 const int HBRIDGE_R_EN = 4;
 const int HBRIDGE_R_IS = 8;
 const int HBRIDGE_LPWM = 3;
 const int HBRIDGE_L_EN = 2;
 const int HBRIDGE_L_IS = 12;
+
 const int TEC_COOL = 1;
 const int TEC_HEAT = 0;
 
 BTS7960 tec(HBRIDGE_R_EN, HBRIDGE_RPWM, HBRIDGE_R_IS, HBRIDGE_L_EN,
             HBRIDGE_LPWM, HBRIDGE_L_IS, DEBUG);
 
-// LCD shield.
+/*
+  LCD shield config.
+*/
 const int LCD_RS = 8;
 const int LCD_EN = 9;
 const int LCD_D4 = 4;
@@ -44,29 +52,48 @@ const int LCD_BL = 10; // Backlight.
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
+/*
+  Setup.
+*/
 void setup() {
   Serial.begin(9600);
 
   Serial.println("--------------------------------------");
-  Serial.println("fennecPRC v1.0");
+  Serial.print("fennecPRC v");
+  Serial.println(VERSION, 1);
   Serial.println("Programmable Recrystallization Chamber");
   Serial.println("https://fennecfox.io/");
   Serial.println("--------------------------------------");
 
+  /*
+    Initialise thermometer.
+  */
   sensors.begin();
   sensors.getAddress(thermometerDeviceAddress, 0);
   sensors.setResolution(thermometerDeviceAddress, TEMPERATURE_PRECISION);
 
+  /*
+    Initialise TEC.
+  */
   tec.begin();
   tec.stop(); // Stop the TEC on reset.
-  lcd.begin(16, 2);
 
+  /*
+    Initialise LCD.
+  */
+  lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  lcd.print("  fennecPRC v1  ");
+  lcd.print("fennecPRC v");
+  lcd.print(VERSION, 1);
 }
 
+/*
+  Main loop.
+*/
 void loop() {
-  // Display temperature.
+  /*
+    Display temperature.
+  */
   sensors.requestTemperatures();
 
   float tempC = sensors.getTempCByIndex(0);
@@ -79,12 +106,13 @@ void loop() {
     lcd.print(tempC);
     lcd.print("C");
   } else {
-    lcd.print("Thermo error!");
+    lcd.print("Temp error!");
   }
 
-  // Push-buttons.
-  int button;
-  button = analogRead(0);
+  /*
+    Push-buttons.
+  */
+  int button = analogRead(0);
 
   // Right.
   if (button < 60) {
